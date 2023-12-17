9314dbf9d8de8ec21994f976b3166cb4e507a098
Dezarnaud-Julian <julian.dezarnaud@etu.unice.fr>
2023-12-17 16:11:24 +0100

    sounds

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -2,11 +2,8 @@ import SceneComponent from 'babylonjs-hook';
 import "./App.css";
 import { SkiSlalomGame } from '../../games/ski-slalom/SkiSlalomGame';
 import { Game } from '../../games/Game';
-import { useEffect } from 'react';
 
 function App() {
-
-
   const game: Game = new SkiSlalomGame();
   return (
     <SceneComponent adaptToDeviceRatio autoFocus antialias onSceneReady={game.onStart} onRender={game.onUpdate} id="game-canvas" width={window.innerWidth} height={window.innerHeight} />
--- frontend/src/types.d.ts
+++ frontend/src/types.d.ts
@@ -7,3 +7,7 @@ declare module "*.png" {
   const value: any;
   export = value;
 }
+declare module "*.wav" {
+  const value: any;
+  export = value;
+}
